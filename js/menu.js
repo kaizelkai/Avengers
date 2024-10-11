e183596c598b84d3408cbaf7ff5fcf4ac80ba6ab
KABRE EMMANUEL <150448669+kaizelkai@users.noreply.github.com>
2024-10-11 18:07:18 +0000

    effet scroll

--- js/menu.js
+++ js/menu.js
@@ -6,3 +6,20 @@ function toggleMenu() {
     const isExpanded = navMenu.classList.contains('open');
     document.querySelector('.menu-icon').setAttribute('aria-expanded', isExpanded);
 }
+
+// Script pour le défilement fluide
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault(); // Empêche le comportement par défaut
+        const targetId = this.getAttribute('href').substring(1); // Récupère l'ID de la section cible
+        const targetElement = document.getElementById(targetId);
+
+        // Défilement fluide vers la section cible
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
